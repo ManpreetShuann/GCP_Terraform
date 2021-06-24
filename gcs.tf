6d187db621fb292958ed2f8854b21032923bde10
ManpreetShuann <manpreetshuann@gmail.com>
2021-06-25 00:03:02 +0530

    Added new bucket

--- gcs.tf
+++ gcs.tf
@@ -4,3 +4,10 @@ module "test-bucket" {
   project_id  = var.project_name
   iam_members = []
 }
+
+module "test-bucket-2" {
+  source      = "./module/gcs"
+  name        = "${var.project_name}-test-bucket-2"
+  project_id  = var.project_name
+  iam_members = []
+}
