ff4917fa6f7af82d1d444185dbc4f57e9907a82b
ManpreetShuann <manpreetshuann@gmail.com>
2021-06-26 13:09:36 +0530

    Renamed bucket for branch test

--- gcs.tf
+++ gcs.tf
@@ -1,6 +1,6 @@
-module "test-bucket" {
+module "test-bucket-1" {
   source      = "./module/gcs"
-  name        = "${var.project_name}-test-bucket"
+  name        = "${var.project_name}-test-bucket-1"
   project_id  = var.project_name
   iam_members = []
 }
