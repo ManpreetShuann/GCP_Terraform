235348f4febdcbfd07411036b0038d69cf6a3e86
ManpreetShuann <manpreetshuann@gmail.com>
2021-06-26 14:21:18 +0530

    Added role

--- gcs.tf
+++ gcs.tf
@@ -1,6 +1,11 @@
 module "test-bucket-1" {
-  source      = "./module/gcs"
-  name        = "${var.project_name}-test-bucket-1"
-  project_id  = var.project_name
-  iam_members = []
+  source     = "./module/gcs"
+  name       = "${var.project_name}-test-bucket-1"
+  project_id = var.project_name
+  iam_members = [
+    {
+      role   = "roles/roles/storage.objectViewer"
+      member = "serviceAccount:jenkins@secops-316111.iam.gserviceaccount.com"
+    }
+  ]
 }
